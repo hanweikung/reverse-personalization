9e04b94f2cf3aee2d435a60de97dd17277ab8c97
Han-Wei Kung <hanwei.kung@unitn.it>
2025-05-08 11:34:19 +0200

    Added additional examples and adjusted the hyperparameters

--- submit_nersemble_sdxl.sh
+++ submit_nersemble_sdxl.sh
@@ -25,22 +25,32 @@ export SCRIPT=main_run_sdxl.py
 # Define face images for different runs
 declare -A FACE_IMAGES_MAP
 FACE_IMAGES_MAP["set1"]=" \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/037/37_237.png \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/037/37_355.png \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/037/37_414.png \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/037/37_532.png \
+ /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/037/37_355.png \
+ /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/037/37_414.png \
+ /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/037/37_532.png \
 "
 FACE_IMAGES_MAP["set2"]=" \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/099/99_229.png \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/099/99_286.png \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/099/99_343.png \
-  /leonardo_work/IscrC_SYNFACE/hkung/datasets/nersemble_data/data/selected/whole_face_no_align/099/99_457.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/038/38_145.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/038/38_193.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/038/38_697.png \
+"
+FACE_IMAGES_MAP["set3"]=" \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/042/42_037.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/042/42_208.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/042/42_262.png \
+"
+FACE_IMAGES_MAP["set4"]=" \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/099/99_229.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/099/99_286.png \
+  /leonardo_work/IscrC_SYNFACE/hkung/experiments3/datasets/nersemble/data/selected/whole_face_no_align/099/99_457.png \
 "
 
 # Map set keys to their respective negative prompts
 declare -A NEGATIVE_PROMPTS
 NEGATIVE_PROMPTS["set1"]="28-year-old indian man"
-NEGATIVE_PROMPTS["set2"]="28-year-old middle eastern man"
+NEGATIVE_PROMPTS["set2"]="23-year-old asian girl"
+NEGATIVE_PROMPTS["set3"]="30-year-old asian woman"
+NEGATIVE_PROMPTS["set4"]="28-year-old middle eastern man"
 
 run_script() {
   local input_image=$1
@@ -50,9 +60,9 @@ run_script() {
   python $SCRIPT \
     --sd_model_path "stabilityai/stable-diffusion-xl-base-1.0" \
     --input_image $input_image \
-    --guidance_scale "-8.0" \
+    --guidance_scale "-4.0" \
     --skip "0.7" \
-    --ip_adapter_scale "0.2" \
+    --ip_adapter_scale "0.4" \
     --id_emb_scale "1.0" \
     --inversion "leditspp" \
     --num_inversion_steps "100" \
